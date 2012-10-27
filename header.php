22e1f17d2c74889bd1380a68a2a417d5908fa6cb
altryne <git@alexw.me>
2012-10-27 04:49:25 +0200

    changed header fonts

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
-<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+<script src="http://use.edgefonts.net/source-sans-pro.js"></script>
 <?php
 	/* We add some JavaScript to pages with the comment form
 	 * to support sites with threaded comments (when in use).
